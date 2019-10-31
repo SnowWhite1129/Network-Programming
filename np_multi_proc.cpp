f63aa1722e8709eea6dc8afa8311a70870e705c6
Wei-Gang Sun <gary8511@gmail.com>
2019-10-31 19:21:08 +0800

    OK

--- np_multi_proc.cpp
+++ np_multi_proc.cpp
@@ -11,6 +11,13 @@
 #include <fcntl.h>
 #include <netinet/in.h>
 #include "npshell.h"
+
+#define MAXLIST 1000
+
+using namespace std;
+
+command cmd[MAXLIST];
+
 int main(int argc, char *argv[]){
     signal(SIGCHLD, childHandler);
 
@@ -24,7 +31,7 @@ int main(int argc, char *argv[]){
     struct sockaddr_in servaddr, cli;
 
     // socket create and verification
-    sockfd = socket(PF_INET, SOCK_STREAM, 0);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
         printf("socket creation failed...\n");
         exit(0);
@@ -33,9 +40,9 @@ int main(int argc, char *argv[]){
     bzero(&servaddr, sizeof(servaddr));
 
     // assign IP, PORT
-    servaddr.sin_family = AF_INET;s
-    servaddr.sin_addr.s_addr = htonl("127.0.0.1");
-    servaddr.sin_port = htons(7001);
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(chartoint(argv[0]));
 
     // Binding newly created socket to given IP and verification
     if ((bind(sockfd, (sockaddr *)&servaddr, sizeof(servaddr))) != 0) {
@@ -63,6 +70,13 @@ int main(int argc, char *argv[]){
         if (pid == 0){
             close(sockfd);
             // Function for chatting between client and server
+            for (int i = 0; i < 3; ++i) {
+                close(i);
+            }
+            dup2(sockfd, STDIN_FILENO);
+            dup2(sockfd, STDOUT_FILENO);
+            dup2(sockfd, STDERR_FILENO);
+
             func(connfd);
             exit(0);
         }
