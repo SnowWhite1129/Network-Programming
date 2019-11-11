29807088e5ac8fa03614269e2467fe5ed170530a
Wei-Gang Sun <gary8511@gmail.com>
2019-11-11 15:58:40 +0800

    init

--- np_single_proc.cpp
+++ np_single_proc.cpp
@@ -370,7 +370,7 @@ int main(int argc, char *argv[]){
             {
                 dup2(sd, STDIN_FILENO);
                 dup2(sd, STDOUT_FILENO);
-                dup2(sd, STDERR_FILENO);
+                //dup2(sd, STDERR_FILENO);
                 chat(cli, i);
             }
         }
