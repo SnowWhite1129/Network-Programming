d5615d5b9aacfe9dce70fa7a9f22173daa17fe8a
Wei-Gang Sun <gary8511@gmail.com>
2019-11-18 11:38:56 +0800

    789

--- nppipe.cpp
+++ nppipe.cpp
@@ -1,16 +1,13 @@
 #include "nppipe.h"
 #include <unistd.h>
 
-bool checkPipeExist(int sender, int clientID, Pipe pipe_table[][max_clients]){
-    return pipe_table[sender][clientID].readfd != -1 || pipe_table[sender][clientID].writefd != -1;
-}
-bool checkPipeStatus(int sender, int clientID, Pipe pipe_table[][max_clients]){
+bool checkPipeStatus(int sender, int clientID, const Pipe pipe_table[][max_clients]){
     return pipe_table[sender][clientID].readfd == -1 || pipe_table[sender][clientID].writefd == -1;
 }
-bool checkPipeStatusMulti(int sender, int clientID, bool pipe_status[][max_clients]){
+bool checkPipeStatusMulti(int sender, int clientID, const bool pipe_status[][max_clients]){
     return pipe_status[sender][clientID];
 }
-void closePipe(int clientID, Pipe pipe_table[][max_clients]){
+void closePipe(int clientID, const Pipe pipe_table[][max_clients]){
     for (int i = 0; i < max_clients; ++i) {
         if (pipe_table[i][clientID].readfd != -1 && pipe_table[i][clientID].writefd != -1){
             close(pipe_table[i][clientID].readfd);
--- nppipe.h
+++ nppipe.h
@@ -6,8 +6,7 @@
 struct Pipe{
     int readfd, writefd;
 };
-bool checkPipeExist(int sender, int clientID, Pipe pipe_table[][max_clients]);
-bool checkPipeStatus(int sender, int clientID, Pipe pipe_table[][max_clients]);
-void closePipe(int clientID, Pipe pipe_table[][max_clients]);
-bool checkPipeStatusMulti(int sender, int clientID, bool pipe_status[][max_clients]);
+bool checkPipeStatus(int sender, int clientID, const Pipe pipe_table[][max_clients]);
+void closePipe(int clientID, const Pipe pipe_table[][max_clients]);
+bool checkPipeStatusMulti(int sender, int clientID, const bool pipe_status[][max_clients]);
 #endif //NETWORK_PROGRAMMING_NPPIPE_H
